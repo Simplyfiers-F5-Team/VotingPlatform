cabab04f4c0b596370ed78685df154bc4f10a77c
PablinVaz <pablin_vaz@outlook.es>
2021-05-17 14:57:52 +0200

    finnish de verdad XD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,10 +10,10 @@
 </head>
 <body class="bgMorado textWhite">
 <main>
-    <h1><img class="logobasetis" src="./img/Basetis_Logotipo.svg" alt="Logo Basetis"></h1>
+    <h1 class="pb-4"><img class="logobasetis" src="./img/Basetis_Logotipo.svg" alt="Logo Basetis"></h1>
     <h2 class="fontFam">Bienvenido a la <br/> plataforma de votación</h2>
-    <section>
-        <form action="" method="post" class="px-3">
+    <section class="pb-4">
+        <form action="" method="post" class="pt-3 px-3">
             <fieldset class="borderColorRosa p-3 d-grid gap-2">
                 <label for="employee" class="fontFam h4">Escoge tu próxima formación</label>
                 <input type="text" name="employee" id="employee" class="" placeholder="Identificación del trabajador">
@@ -23,7 +23,7 @@
     </section>
     <section class="pt-5">
         <form action="" method="post" class="px-3">
-            <fieldset class="borderColorRosa p-2 d-grid gap-2">
+            <fieldset class="borderColorRosa p-3 d-grid gap-2">
                 <p class="fontFam h4">¿Eres formador?</p>
                 <a href="{{ route('home') }}" class="btn bgRosa fontFam textWhite">LOGIN</a>
             </fieldset>
