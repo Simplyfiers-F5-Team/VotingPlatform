2e61b2c1c11f68c1a3b7021c8a656d1460336946
Arturo <arturomf88@gmail.com>
2021-05-18 09:00:40 +0200

    Course model, controller and routes

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Course;
 
 class CourseController extends Controller
 {
-    //
+    public function index()
+    {
+        $courses = Course::orderBy('name')->get();
+        return view('courses.index', ['courses' => $courses]);
+    }
+
+    public function show()
+    {
+        return view('courses.show');
+    }
+
+    public function create()
+    {
+        return view ('courses.create');
+    }
+
+    public function store()
+    {
+        $course = new Course();
+        $course->name = request('name');
+        $course->description = request('description');
+
+        $course->save();
+    }
+
+    public function destroy($id)
+    {
+        $course = Course::findOrFail($id);
+        $course->delete();
+    }
+
 }
