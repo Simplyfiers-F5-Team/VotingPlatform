ed3ee24577e2a5d7a79dc1c433bc5d785abadbdd
PablinVaz <pablin_vaz@outlook.es>
2021-05-21 10:08:24 +0200

    edit and create.blade css

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -18,20 +18,25 @@
         </button>
     </div>
     @enderror
-    <div class="wrapper">
-        <h1>Create a Course</h1>
-        <form action="{{ route('courses.store') }}" method="POST">
-            @csrf
-            <label for="name">Course name:</label>
-            <input type="text" name="name" id="name" value="{{old('name')}}">
+    <div class="container card bgMorado borderColorRosa">
+        <div class="mt-3">
+            <h2 class="h1 m-0">Create a Course</h2>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('courses.store') }}" method="POST">
+                @csrf
+                <div>
+                    <label for="name" class="h4">Course name:</label>
+                    <input type="text" name="name" id="name" value="{{old('name')}}" class="form-control">
+                </div>
 
-            <label for="description">Short description:</label>
-            <textarea name="description" id="description" cols="30" rows="10">{{old('description')}}</textarea>
-
-
-            <input type="submit" value="Add course" class="btn bgRosa fontFam textWhite">
-        </form>
-        <a href="{{ route('home') }}" class="btn bgRosa fontFam textWhite">Back</a>
+                <div class="my-4">
+                    <label for="description" class="h4">Short description:</label>
+                    <textarea name="description" id="description" cols="30" rows="10" class="form-control">{{old('description')}}</textarea>
+                </div>
+                <input type="submit" value="Add course" class=" mb-4 btn bgRosa fontFam textWhite">
+            </form>
+        <a href="{{ route('home') }}" class=" btn bgRosa fontFam textWhite">Back</a>
     </div>
 @endsection
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,16 +14,13 @@
             <div class="col-md-8">
                 <div class="card bgMorado borderColorRosa">
                     <div class="card-header borderColorRosa">{{ __('Dashboard') }}</div>
-
                     <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
                             </div>
                         @endif
-
                         {{ __('Welcome admin') }}
-
                         <a href="{{ route('courses.create') }}" class="btn bgRosa fontFam textWhite">Create course</a>
                         <a href="{{ route('courses.index') }}" class="btn bgRosa fontFam textWhite">Courses batch</a>
                     </div>
