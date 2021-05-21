0f2e179455e55756bfff2f6fa86f8313faa8bef2
PablinVaz <pablin_vaz@outlook.es>
2021-05-21 09:20:57 +0200

    errores

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -1,20 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="wrapper ">
+
+    @error("name")
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    *{{$message}}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    @enderror
+    @error("description")
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    *{{$message}}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    @enderror
+    <div class="wrapper">
         <h1>Create a Course</h1>
         <form action="{{ route('courses.store') }}" method="POST">
             @csrf
             <label for="name">Course name:</label>
             <input type="text" name="name" id="name" value="{{old('name')}}">
-            @error("name")
-            <small>*{{$message}}</small>
-            @enderror
+
             <label for="description">Short description:</label>
             <textarea name="description" id="description" cols="30" rows="10">{{old('description')}}</textarea>
-            @error("description")
-            <small>*{{$message}}</small>
-        @enderror
+
 
             <input type="submit" value="Add course" class="btn bgRosa fontFam textWhite">
         </form>
--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+    @error("name")
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        *{{$message}}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    @enderror
+    @error("description")
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        *{{$message}}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    @enderror
     <div class="wrapper ">
         <h1>Edit a Course</h1>
         <form action="{{ route('courses.update', $course) }}" method="post">
@@ -8,15 +24,8 @@
             @method('put')
             <label for="name">Course name:</label>
             <input type="text" name="name" id="name" value="{{old('name', $course->name)}} ">
-            @error("name")
-            <small>*{{$message}}</small>
-            @enderror            
             <label for="description">Short description:</label>
             <textarea name="description" id="description" cols="15" rows="10">{{old('description', $course->description)}}</textarea>
-            @error("description")
-            <small>*{{$message}}</small>
-            @enderror
-            
             <input type="submit" value="Update course" class="btn bgRosa fontFam textWhite">
         </form>
         <a href="{{ route('home') }}" class="btn bgRosa fontFam textWhite">Back</a>
