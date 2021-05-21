8652813407a708fd02df2251ad90cec5ac9d8cea
Juan Carlos <sedenojc@gmail.com>
2021-05-21 09:40:25 +0200

    mensaje crear
    corrección mensaje de ccrear

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('mssg') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
+    @if (session('mssg'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('mssg') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
